4870cfc91d1cb35cc4949bb7aa3ca3fec9e0d128
Gleb Danilenko <glebdanilenko@gmail.com>
2019-03-17 14:00:18 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var pr = document.getElementById ('price');
+var vol = document.getElementById ('vol');
+var fuel = document.getElementById ('fuel');
+var age = document.getElementById ('age');
+var eu = document.getElementById ('euro');
+
+function calc () {
+    var total = 0;
+    total = 
+}
