67b833a401686bd9d6df7a2d105c84519ba09876
Gleb Danilenko <glebdanilenko@gmail.com>
2019-03-17 18:27:55 +0200

    2iter

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
-var pr = document.getElementById ('price');
-var vol = document.getElementById ('vol');
-var fuel = document.getElementById ('fuel');
-var age = document.getElementById ('age');
-var eu = document.getElementById ('euro');
+var year = new Date();
+var fuel = 0;
 
-function calc () {
+function calc() {
+
+    var pr = document.getElementById('price').value;
+    var vol = document.getElementById('vol').value;
+    var fuel = document.getElementById('fuel').value;
+    var age = document.getElementById('age').value;
+    var eu = document.getElementById('euro').value;
     var total = 0;
-    total = 
+    var akz = 0;
+    var y = (vol / 1000) * (year.getUTCFullYear() - age);
+
+    if (fuel == 1) {
+        if (vol < 3000) {
+            akz = (y * 50) / eu;
+        } else if (vol >= 3000) {
+            akz = (y * 100) / eu;
+        }
+    } else if (fuel == 2) {
+        if (vol <= 3500) {
+            akz = (y * 75) / eu;
+        } else if (vol > 3500) {
+            akz = (y * 150) / eu;
+        }
+    }
+    var poh = pr / 10;
+    var fullprice = akz + (pr * 1) + poh;
+    var nds = (fullprice / 100) * 20;
+    total = nds + akz + poh;
+    total=Math.round(total);
+    console.log(total);
+
+
+
+
 }
